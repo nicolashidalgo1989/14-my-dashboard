50f6db723ef9543019dd3b67108ad4bd9cb0ec8d
Nicolas Hidalgo <38006165+nicolashidalgo1989@users.noreply.github.com>
2024-02-19 01:49:07 -0300

    feat: alias tsconfig folders

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { RouterOutlet } from '@angular/router'; 
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
 
 @Component({
   standalone: true,
