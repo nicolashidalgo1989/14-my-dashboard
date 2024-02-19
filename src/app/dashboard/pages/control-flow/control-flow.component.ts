1e6eeca89db18c6ef75d7ff8038845f7ba420597
Nicolas Hidalgo <38006165+nicolashidalgo1989@users.noreply.github.com>
2024-02-19 02:16:35 -0300

    feat: esttructura  de  control for/empty

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -11,6 +11,8 @@ export default class ControlFlowComponent {
   
   public showContent = signal(false);
   public grade = signal<Grade>('F');
+  public frameworks = signal(['Angular','Vue','React','Svelte','Qwik']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update( value => !value )
