be8bc8f10b7d29e0d5f5fbbfa5dd40633d54ce55
Nicolas Hidalgo <38006165+nicolashidalgo1989@users.noreply.github.com>
2024-02-19 01:35:40 -0300

    feat: sidemenu init

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
 
 @Component({
   standalone: true,
-  imports: [ RouterOutlet ],
+  imports: [ RouterOutlet, SidemenuComponent ],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
-  selector: 'app-sidemenu',
+  selector: 'sidemenu',
   standalone: true,
   imports: [],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
 export class SidemenuComponent {
+  
+  public menuItems = routes
+    .map( route => route.children ?? [] )
+    .flat()
+    .filter( route => route && route.path )
+    .filter( route => !route.path?.includes(':'));
+
+  constructor(){
+
+    // const dashboardRoutes = routes
+    //   .map( route => route.children ?? [] )
+    //   .flat()
+    //   .filter( route => route && route.path )
+    //   .filter( route => !route.path?.includes(':'))
+
+    // console.log(dashboardRoutes);
+    
+
+  } 
 
 }
