76c40fd6f04e80bcefa37004c842898c2a0d11e7
Nicolas Hidalgo <38006165+nicolashidalgo1989@users.noreply.github.com>
2024-02-22 12:36:31 -0300

    feat: defer trigger on interaction hover timer immediate idle

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, HeavyLoadersFastComponent, TitleComponent ],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'heavy-loaders-fast',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <h1>Heavy Loaders Fast</h1>
+    <section [ngClass]="['w-full', cssClass]">
+      <ng-content />
+    </section>
   `,
 })
 export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor(){
+    console.log('Heavy Loader creado!');
+
+  }
 }
